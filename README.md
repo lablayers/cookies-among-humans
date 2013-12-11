d2cd958fb2d89b27a4a0d48d6800d71c4f0f21b0
Victor Lourng <hello@victorlourng.com>
2013-12-10 19:51:54 -0800

    Hello GitHub

---
+++ README.md
@@ -0,0 +1,12 @@
+# Cookies Among Humans
+
+To install, cd into the repo directory and run
+
+    $ npm install
+    $ sudo npm install grunt-cli
+
+To run locally, try
+
+    $ grunt && node app.js
+
+Then open up this: http://localhost:3000
---
+++ src/socket.js
@@ -0,0 +1,38 @@
+!function (angular, io, app) {
+  'use strict';
+  
+  app.factory('socket', ['$rootScope',
+  function ($rootScope) {
+    var socket = io.connect()
+  
+    return {
+      on: function (name, callback) {
+        socket.on(name, function () {
+          var args = arguments
+        
+          $rootScope.$apply(function () {
+            callback.apply(socket, args)
+          })
+        })
+      }
+    , emit: function () {
+        var callback = arguments[arguments.length - 1]
+          , args
+      
+        if (typeof callback === 'function') {
+          args = [].slice.call(arguments, 0, -1)
+
+          socket.emit.apply(socket, args.concat(function () {
+            var args = arguments
+          
+            $rootScope.$apply(function () {
+              callback.apply(socket, args)
+            })
+          }))
+        } else {
+          socket.emit.apply(socket, arguments)
+        }
+      }
+    }
+  }])
+}(window.angular, window.io, window.app)
